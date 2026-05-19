d2f41191a487fc02f68fd5aa1402e1c1f99e831b
David Matijevic Goodman <149710898+davidgoodman333@users.noreply.github.com>
2026-05-19 22:42:40 +0200

    Test RealBeans Shopify storefront

--- cypress/e2e/realbeans.cy.js
+++ cypress/e2e/realbeans.cy.js
@@ -1,16 +1,48 @@
-describe('RealBeans shop basic checks', () => {
-  it('loads the Cypress example page', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('Kitchen Sink').should('be.visible')
+describe('RealBeans Shopify store checks', () => {
+  const storeUrl = 'https://realbeans-4.myshopify.com'
+  const password = 'shiapu'
+
+  beforeEach(() => {
+    cy.on('uncaught:exception', (err) => {
+      if (err.message.includes('ResizeObserver loop')) {
+        return false
+      }
+    })
   })
 
-  it('shows the Commands section', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('Commands').should('be.visible')
+  function enterStorePassword(path = '/') {
+    cy.visit(`${storeUrl}${path}`)
+
+    cy.get('body').then(($body) => {
+      if ($body.text().includes('Enter using password')) {
+        cy.contains('Enter using password').click()
+        cy.get('input[type="password"]').type(password)
+        cy.get('button[type="submit"]').click()
+      }
+    })
+
+    cy.url().should('include', 'realbeans-4.myshopify.com')
+  }
+
+  it('loads the RealBeans homepage', () => {
+    enterStorePassword('/')
+    cy.get('body').should('contain.text', 'RealBeans')
+    cy.get('body').should('contain.text', 'Welcome to our store')
   })
 
-  it('has a GitHub link', () => {
-    cy.visit('https://example.cypress.io')
-    cy.contains('GitHub').should('be.visible')
+  it('shows the product catalog', () => {
+    enterStorePassword('/collections/all')
+
+    cy.get('body').should('contain.text', 'RealBeans Espresso Blend')
+    cy.get('body').should('contain.text', 'RealBeans House Roast')
+    cy.get('body').should('contain.text', 'RealBeans Decaf Roast')
+  })
+
+  it('has an About page link in the storefront navigation', () => {
+    enterStorePassword('/')
+
+    cy.get('a[href="/pages/about-realbeans"]')
+      .should('exist')
+      .and('contain.text', 'About')
   })
 })
